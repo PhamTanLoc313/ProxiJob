5eacd8406edac2c21ba0867435c09066f52281c4
@fsa_LocPT <locptse184400@fpt.edu.vn>
2026-07-04 14:11:36 +0700

    Fix conflict of payrool screen

--- src/ProxiJob_Mobile/src/context/useAuth.js
+++ src/ProxiJob_Mobile/src/context/useAuth.js
@@ -80,7 +80,7 @@ export const useAuth = ({
 
   useEffect(() => {
     if (user) {
-      const premiumTiers = ['Enterprise', 'Premium', 'Standard'];
+      const premiumTiers = ['Enterprise', 'HRM Basic'];
       setIsEnterprise(premiumTiers.includes(user?.subscriptionTier));
     } else {
       setIsEnterprise(false);
